3fd2c737cd63b738e65427b5cde76bbb20dc4774
mzhaonanq <1002604003@qq.com>
2020-11-21 21:08:51 +0800

    完成add方法的重构

--- cli.js
+++ cli.js
@@ -2,27 +2,12 @@ const { program } = require('commander');
 program.version('0.0.1');
 const api = require("./index")
 
-
-program
-    .command('print <content> [destination]')
-    .description('print string')
-    .action((content, destination) => {
-        console.log(content);
-    });
-
-program
-    .command('say')
-    .description('say something')
-    .action((...content) => {
-        const words = content.slice(1)[0].join(" ")
-        console.log(words);
-    });
-
 program
     .command('add <task>')
-    .description('add something')
+    .description('add task')
     .action((task) => {
         api.add(task)
     });
+
 
 program.parse(process.argv);
--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-const homePath = process.env['HOME']
-const path = require('path')
-const fs = require("fs")
-module.exports.add = (title) => {
-    fs.readFile(path.join(homePath, ".todo"), { flag: "a+" }, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            let list
-            try {
-                list = JSON.parse(data.toString())
-            } catch (err2) {
-                list = []
-            }
-            console.log(data.toString())//字符串类型的数组
-            console.log(list)//js数组
-            const task = {
-                title: title,
-                done: false
-            }
-            list.push(task)
-            console.log(list)//添加新任务后的新数组
-            let jsonString = JSON.stringify(list)
-            fs.writeFile(path.join(homePath, ".todo"), jsonString + '\n', (err3) => {
-                if (err2) {
-                    console.log(err3)
-                }
-            })
-        }
-    })
+const db = require("./db")
+
+module.exports.add = async (title, path) => {
+    const list = await db.read(path)
+    list.push({ title, done: false })
+    await db.write(list, path)
 }
+
+module.exports.clear = async () => {
+    await db.write([])
+}
+
