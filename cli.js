6fd75afb4cfae1f06bcd3077f36089b6d2bbfe01
mzhaonanq <1002604003@qq.com>
2020-11-21 16:12:16 +0800

    简单command

--- cli.js
+++ cli.js
@@ -1,14 +1,21 @@
 const { program } = require('commander');
 program.version('0.0.1');
 
+
+
 program
-    .option('-d, --debug', 'output extra debugging')
-    .option('-s, --pizza-size <size>', 'size of pizza')//small,medium,large
-    .option('-t, --pizza-type <type>', 'flavour of pizza');//sausage,onion, mushrooms,curry,cheese
+    .command('print <content> [destination]')
+    .description('print string')
+    .action((content, destination) => {
+        console.log(content);
+    });
+
+program
+    .command('say')
+    .description('say something')
+    .action((...content) => {
+        const words = content.slice(1)[0].join(" ")
+        console.log(words);
+    });
 
 program.parse(process.argv);
-
-if (program.debug) console.log(program.opts());
-console.log('pizza details:');
-if (program.pizzaSize) console.log(`- ${program.pizzaSize}`);
-if (program.pizzaType) console.log(`- ${program.pizzaType}`);
